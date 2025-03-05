62fe53d75d09970e6d03e4c7154b571a08008a4f
trann <trannhukhoinguyen@gmail.com>
2025-03-05 14:59:25 +0700

    remove language

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,6 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import config from "./src/config/config.json";
-import languagesJSON from "./src/config/language.json";
-
-const { default_language } = config.settings;
-
-const supportedLang = [...languagesJSON.map((lang) => lang.languageCode)];
-const disabledLanguages = config.settings.disable_languages;
-
-// Filter out disabled languages from supportedLang
-const filteredSupportedLang = supportedLang.filter(
-    (lang) => !disabledLanguages.includes(lang),
-);
 
 // https://astro.build/config
 export default defineConfig({
@@ -23,16 +11,11 @@ export default defineConfig({
   },
   prefetch: true,
   site: 'https://patriarchalzen.quest',
-  trailingSlash: config.site.trailing_slash ? "always" : "ignore",
   integrations: [
     react(),
     sitemap(),
     tailwind(),
   ],
-  i18n: {
-    locales: filteredSupportedLang,
-    defaultLocale: default_language,
-  },
   experimental: {
     svg: true,
   },
